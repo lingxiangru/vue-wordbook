476d328dea46624581015f6957e2d0ad10dfd442
Martin <2906935779@qq.com>
2020-06-21 18:23:28 +0800

    首页完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,10 +1,22 @@
 <template>
-	<div class="container" style="height: 500px;">
-		<div id="top_box" class="rounded">
-			<!-- <div id="inner_box" class="rounded text-center bg-white py-1 d-flex align-items-center">
-				<h3>单词本</h3>
-			</div> -->
-			<!-- <h5 class="text-center text-white">English Word Book</h5> -->
+	<div class="container border py-5 mt-5" style="width: 500px;">
+		<div id="top_box" class="rounded text-center p-5">
+			<h3 class="bg-white w-25 rounded p-1 m-auto">单词本</h3>
+			<h3 class="text-white font-weight-light">English Word Book</h3>
+		</div>
+		<form class="mt-5">
+			<div class="form-group">
+				<label for="username">UserName:</label>
+				<input type="text" id="username" class="form-control" placeholder="请输入用户名">
+			</div>
+			<div class="form-group">
+				<label for="userpwd">Password:</label>
+				<input type="password" id="userpwd" class="form-control" placeholder="请输入密码">
+			</div>
+			<button type="button" class="btn btn-primary w-100 mt-2">登录</button>
+		</form>
+		<div class="d-flex justify-content-end mt-2">
+			<router-link to="/register">注册</router-link>
 		</div>
 	</div>
 </template>
@@ -16,6 +28,5 @@
 	#top_box {
 		background-image: url(../assets/1.jpg);
 		background-repeat: no-repeat;
-		height: 200px;
 	}
 </style>
